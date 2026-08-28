6517547fa254c61b7bd76dde3d3593186c111427
Emina Demura <coluk.emi0729@gmail.com>
2026-08-28 19:10:38 +0900

    feat:シーダーの実装(Seeder) #19

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+use App\Models\Tag;
+use App\Models\Category;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 20; $i++) {
+        //お問い合わせを20件作成
+            $contacts = Contact::create([
+                'first_name' => fake('ja_JP')->firstName(),
+                'last_name' => fake('ja_JP')->lastName(),
+                //genderは1:男性、2:女性、3:その他
+                'gender' => fake('ja_JP')->randomElement([1,2,3]),
+                'email' => fake('ja_JP')->unique()->safeEmail(),
+                'tel' => fake('ja_JP')->unique()->phoneNumber(),
+                'address' => fake('ja_JP')->address(),
+                'building' => fake('ja_JP')->secondaryAddress(),
+                // カテゴリを関連付ける
+                'category_id' => Category::inRandomOrder()->first()->id,
+                'detail' => fake('ja_JP')->text(120),
+            ]);
+
+            // タグを1〜3個ランダムで付与（仕様書の要件）
+            $tags = Tag::inRandomOrder()->take(rand(1,3))->pluck('id');
+            $contacts->tags()->attach($tags);
+        }
+    }
+}
